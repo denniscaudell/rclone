b15b40786e01347108b28220494ad24a72302fe6
Nick Craig-Wood <nick@craig-wood.com>
2018-05-03 09:45:24 +0100

    mount,cmount: make --noappledouble --noapplexattr and change defaults #2287
    Before this change we would unconditionally set the OSXFUSE options
    noappledouble and noapplexattr.
    
    However the noapplexattr options caused problems with copies in the
    Finder.
    
    Now the default for noapplexattr is false so we don't add the option
    by default and the user can override the defaults using the
    --noappledouble and --noapplexattr flags.

--- cmd/mountlib/mount.go
+++ cmd/mountlib/mount.go
@@ -31,6 +31,8 @@ var (
 	ExtraFlags         []string
 	AttrTimeout        = 1 * time.Second // how long the kernel caches attribute for
 	VolumeName         string
+	NoAppleDouble      = true  // use noappledouble by default
+	NoAppleXattr       = false // do not use noapplexattr by default
 )
 
 // Check is folder is empty
@@ -253,6 +255,11 @@ will see all files and folders immediately in this mode.
 	flags.StringArrayVarP(flagSet, &ExtraFlags, "fuse-flag", "", []string{}, "Flags or arguments to be passed direct to libfuse/WinFsp. Repeat if required.")
 	flags.BoolVarP(flagSet, &Daemon, "daemon", "", Daemon, "Run mount as a daemon (background mode).")
 	flags.StringVarP(flagSet, &VolumeName, "volname", "", VolumeName, "Set the volume name (not supported by all OSes).")
+
+	if runtime.GOOS == "darwin" {
+		flags.BoolVarP(flagSet, &NoAppleDouble, "noappledouble", "", NoAppleDouble, "Sets the OSXFUSE option noappledouble.")
+		flags.BoolVarP(flagSet, &NoAppleXattr, "noapplexattr", "", NoAppleXattr, "Sets the OSXFUSE option noapplexattr.")
+	}
 
 	// Add in the generic flags
 	vfsflags.AddFlags(flagSet)
